d024d560d5abc1c53078721f69ccc17df8468e95
Leesungsup <tmdtjq0116@naver.com>
2022-01-24 17:48:18 +0900

    chat

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+const express = require('express');
+const fs =require('fs')
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 let rooms = [];
-
-app.get('/', (req, res) => {
+app.use('/css',express.static('./client/css'))
+app.use('/js',express.static('./client/js'))
+app.get('/',(req,res)=>{
+    fs.readFile('./client/index.html',function(err,data){
+        if(err){
+            res.send(err)
+        }else{
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        }
+    })
+})
+/*app.get('/', (req, res) => {
     res.sendFile(__dirname + '/front.html');
-});
-
+});*/
 io.on('connection', (socket)=>{
+    socket.on('newUser', function(name){
+        console.log(name+' 님이 접속하였습니다.')
+        socket.name=name
+        socket.emit('update',{type:'connect',name:'SERVER',message:name+'님이 접속하였습니다.'})
+    });
+    socket.on('message', function(data){
+        data.name=socket.name
+        console.log("전달된 메시지:",data)
+        socket.broadcast.emit('update',data)
+    });
+    socket.on('disconnect', function () {
+        console.log(socket.name+'user disconnected');
+        socket.broadcast.emit('update',{type:'disconnect',name:'SERVER',message:socket.name+'님이 나가셨습니다.'});
+    });
+});
+/*io.on('connection', (socket)=>{
     socket.on('request_message', (msg) => {
         // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
         io.emit('response_message', msg);
@@ -35,9 +64,9 @@ io.on('connection', (socket)=>{
     socket.on('disconnect', async () => {
         console.log('user disconnected');
     });
-});
+});*/
 
-(async function(){
+/*(async function(){
 })();
 
 function getUserCurrentRoom(socket){
@@ -52,7 +81,7 @@ function getUserCurrentRoom(socket){
         } 
     }
     return currentRoom;
-}
+}*/
 const port = process.env.PORT || 5000
 http.listen(port, () => {
   console.log(`Server Listening on ${port}`)
