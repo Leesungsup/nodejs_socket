cb06b9ca87a0c9eaf6268b1fce3b891ccb829891
Leesungsup <tmdtjq0116@naver.com>
2022-01-30 03:01:35 +0900

    multer

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.get('/',(req,res)=>{
 /*app.get('/', (req, res) => {
     res.sendFile(__dirname + '/front.html');
 });*/
+let socketList = [];
 io.on('connection', (socket)=>{
+    socketList.push(socket);
     socket.on('newUser', function(name){
         console.log(name+' 님이 접속하였습니다.')
         socket.name=name
@@ -47,11 +49,20 @@ io.on('connection', (socket)=>{
         console.log("전달된 메시지:",data)
         socket.broadcast.emit('update',data)
     });
+    socket.on('image', (data)=>{
+        socketList.forEach(function(item, i) {
+            console.log(item.id);
+            if (item != socket) {
+                item.emit('image', data);
+            }
+        }); 
+    })
     socket.on('disconnect', function () {
         console.log(socket.name+'user disconnected');
         socket.broadcast.emit('update',{type:'disconnect',name:'SERVER',message:socket.name+'님이 나가셨습니다.'});
     });
 });
+app.use('/image', require('./multer'));
 /*io.on('connection', (socket)=>{
     socket.on('request_message', (msg) => {
         // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
@@ -99,5 +110,7 @@ function getUserCurrentRoom(socket){
 }*/
 const port = process.env.PORT || 5000
 http.listen(port, () => {
+    var dir = './uploadedFiles';
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
   console.log(`Server Listening on ${port}`)
 });
